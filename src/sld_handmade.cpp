55668209989b826b01fab4ebf2903a14c38a6c3b
syrshax <selvix728@gmail.com>
2025-10-25 15:32:49 +0200

    now we can animate the buffer!

--- src/sld_handmade.cpp
+++ src/sld_handmade.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <sys/mman.h>
+#include <sys/types.h>
 
 #include "SDL3/SDL.h"
 
@@ -8,15 +10,48 @@
 #define local_persist static
 #define global_variable static
 
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+global_variable bool Running = true;
 global_variable SDL_Texture *Texture;
 global_variable void *BitmapMemory;
 global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+global_variable int BitsPerPixel = 4;
+
+internal void RenderWeirdGradiant(int x_offset, int y_offset) {
+  int Pitch = BitmapWidth * BitsPerPixel;
+  uint8 *Row = (uint8 *)BitmapMemory;
+  for (int Y = 0; Y < BitmapHeight; ++Y) {
+    uint32 *Pixel = (uint32 *)Row;
+    for (int X = 0; X < BitmapWidth; ++X) {
+
+      /*
+       * Pixel in memory:	RR	GG	BB	padding
+       * LITTLE ENDIAN ARCHITECTURE
+       * 0x000000
+       */
+      uint8 red = (X + x_offset);
+      uint8 green = (Y + y_offset);
+      uint8 blue = 100;
+      uint8 opacity = 255;
+
+      *Pixel++ = (opacity << 24) | (blue << 16) | (green << 8) | red;
+    }
+    Row += Pitch;
+  }
+}
 
 // TODO: This will be filed
 
-internal void SDLResizeTextureBuffer(SDL_Renderer *r, int height, int width) {
+internal void SDLResizeTextureBuffer(SDL_Renderer *r, int width, int height) {
   if (BitmapMemory) {
-    free(BitmapMemory);
+    int ok =
+        munmap(BitmapMemory, ((BitmapWidth * BitmapHeight) * BitsPerPixel));
+    std::cerr << "Its ok?: " << ok << "\n";
   }
   if (Texture) {
     SDL_DestroyTexture(Texture);
@@ -25,10 +60,11 @@ internal void SDLResizeTextureBuffer(SDL_Renderer *r, int height, int width) {
   Texture = SDL_CreateTexture(r, SDL_PIXELFORMAT_ABGR8888,
                               SDL_TEXTUREACCESS_STREAMING, width, height);
 
-  printf("IM HERE OR NOT");
-
   BitmapWidth = width;
-  BitmapMemory = malloc(width * height * 4);
+  BitmapHeight = height;
+  BitmapMemory =
+      mmap(0, (BitmapWidth * BitmapHeight) * BitsPerPixel,
+           PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 }
 
 internal void SDLUpdateWindow(SDL_Renderer *r) {
@@ -42,9 +78,6 @@ bool HandleEvent(SDL_Event *Event) {
 
   switch (Event->type) {
   case SDL_EVENT_WINDOW_RESIZED: {
-
-    printf("Window RESIZED!\n Size is kinda nuts!: %d x %d\n",
-           Event->window.data1, Event->window.data2);
     SDL_Window *w = SDL_GetWindowFromID(Event->window.windowID);
     SDL_Renderer *r = SDL_GetRenderer(w);
     SDLResizeTextureBuffer(r, Event->window.data1, Event->window.data2);
@@ -52,7 +85,6 @@ bool HandleEvent(SDL_Event *Event) {
 
   } break;
   case SDL_EVENT_WINDOW_CLOSE_REQUESTED: {
-    printf("WE OUT!");
     ShouldQuit = true;
   } break;
 
@@ -79,13 +111,23 @@ int main() {
   if (!r) {
     std::cerr << "Failed to create Renderer\n";
   }
+  SDLResizeTextureBuffer(r, 900, 900);
 
-  for (;;) {
+  int x_offset = 0;
+  int y_offset = 0;
+
+  while (Running) {
     SDL_Event registeredEvent;
-    SDL_WaitEvent(&registeredEvent);
-    if (HandleEvent(&registeredEvent)) {
-      break;
+    while (SDL_PollEvent(&registeredEvent)) {
+      if (HandleEvent(&registeredEvent)) {
+        Running = false;
+      }
     }
+
+    RenderWeirdGradiant(x_offset, y_offset);
+    SDLUpdateWindow(r);
+    ++x_offset;
+    ++y_offset;
   }
 
   SDL_DestroyRenderer(r);
